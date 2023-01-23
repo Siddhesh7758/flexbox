18c07d3a3ebda57c97d200b903a5f766a83628e0
Siddhesh Munfan <munfansiddhesh7758@gmail.com>
2023-01-23 11:10:02 +0530

    done

--- App.js
+++ App.js
@@ -3,18 +3,51 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.red}>
+        <View style={styles.green} />
+        <View style={styles.yellow} />
+        <View style={styles.purple} />
+      </View>
+
+      <View style={styles.white}>
+        <View style={styles.green} />
+        <View style={styles.yellow} />
+        <View style={styles.purple} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  
+  red: {
+    flex: 2,
+    backgroundColor: '#000',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  white: {
+    flex: 2,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  green: {
+    backgroundColor: '#17fc03',
+    width: 50,
+    height: 50,
+  },
+  yellow: {
+    backgroundColor: '#fcdf03',
+    width: 50,
+    height: 50,
+  },
+  purple: {
+    backgroundColor: '#d142f5',
+    width: 50,
+    height: 50,
+  }
 });
